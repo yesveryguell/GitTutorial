671a619237db11e0a878bf70409c6ece828174c4
yesveryguell <72716094+yesveryguell@users.noreply.github.com>
2021-02-17 21:06:39 -0500

    Mensaje nuevo

--- src/co/edu/unbosque/gittutorial/Main.java
+++ src/co/edu/unbosque/gittutorial/Main.java
@@ -3,6 +3,6 @@ package co.edu.unbosque.gittutorial;
 public class Main {
     public static void main(String[] args){
         System.out.println("Perro sarnozo");
-        System.out.println("");
+        System.out.println("Hola amiwis");
     }
 }
