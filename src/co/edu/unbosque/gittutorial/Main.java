4c533d39144ccc52b5b2ed98f108dd77fe79fa3f
Felipe Rojas <jfrojasr@unbosque.edu.co>
2021-02-17 19:54:23 -0500

    Finally Fabainum

---
+++ src/co/edu/unbosque/gittutorial/Main.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.gittutorial;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Perro sarnozo");
+        System.out.println("");
+    }
+}
